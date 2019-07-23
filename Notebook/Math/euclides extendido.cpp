a12c9657871b98ce426ddefa2287813a39c1b592
Wall <>
2019-07-23 14:15:02 -0300

    Agregado Math
    agregados euclides y euclides extendido.

---
+++ Notebook/Math/euclides extendido.cpp
@@ -0,0 +1,8 @@
+/* euclides extendido */
+void extendedEuclid (ll a, ll b){ //a * x + b * y = d
+  if (!b) { x = 1; y = 0; d = a; return;}
+  extendedEuclid (b, a%b);
+  ll x1 = y;
+  ll y1 = x - (a/b) * y;
+  x = x1; y = y1;
+}
