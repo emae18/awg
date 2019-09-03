1ff5d522995b2d75011778da55c7c90179c6eb36
emae <31224275+emae18@users.noreply.github.com>
2019-09-03 13:22:02 -0300

    Update SortByValueMap.cpp

--- Notebook/Emae/SortByValueMap.cpp
+++ Notebook/Emae/SortByValueMap.cpp
@@ -1,2 +1,4 @@
-//StringToUpper
-transform(str.begin(), str.end(), str.begin(), (int(*)(int))toupper);
+//SortMapByValue
+typedef function<bool(pair<string,int> ,pair<string,int>  )> Comparator;
+Comparator comp=[](pair<string,int> e1,pair<string,int> e2){return e1.second<e2.second;};
+set<pair<string,int> >,Comparator> s(all(m),comp);
